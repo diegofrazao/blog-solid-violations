eb1839a57dcdfef1b1b1b8ec0ae6af500ecbb53f
Spartan117 <44972207+renatoIFPB@users.noreply.github.com>
2021-02-12 10:42:08 -0300

    Princípio de Inversão de Dependência att

--- src/main/java/br/edu/ifpb/padroes/service/UsuarioServiceImpl.java
+++ src/main/java/br/edu/ifpb/padroes/service/UsuarioServiceImpl.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class UsuarioServiceImpl implements UsuarioService {
 
-    private IUsuarioDAO usuarioDAO = new IUsuarioDAO("banco.db");
+    private UsuarioDAO usuarioDAO = new UsuarioDAO("banco.db");
 
     @Override
     public void criarUsuario(Usuario usuario) {
